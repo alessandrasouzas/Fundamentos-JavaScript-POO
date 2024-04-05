6f74f9d4d9166f1c770ab0b188af6a40fcca198e
Alessandra Souza <le16bits@gmail.com>
2024-04-05 09:18:52 -0300

    aplicando polimorfismo

--- Admin.js
+++ Admin.js
@@ -1,8 +1,12 @@
 import User from "./User.js";
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {       
         super(nome, email, nascimento, role, ativo)
+    }
+
+    exibirInfos() {
+        return `${this.nome}, ${this.role}, ${this.ativo}`
     }
 
     criarCurso(nomeCurso, vagas) {
