a42d8168483dba3e627394d777af30d7fbfb9add
Alessandra Souza <le16bits@gmail.com>
2024-04-05 08:56:42 -0300

    aplicando encapsulamento na classe User, usando #

--- User.js
+++ User.js
@@ -1,14 +1,20 @@
 export default class User {
+    //# torna o atributo privado
+    #nome
+    #nascimento
+    #email
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome
-        this.nascimento = nascimento
-        this.email = email
-        this.role = role || 'estudante'
-        this.ativo = ativo
+        this.#nome = nome
+        this.#nascimento = nascimento
+        this.#email = email
+        this.#role = role || 'estudante'
+        this.#ativo = ativo
     }
 
     exibirInfos(){
-        return `${this.nome}, ${this.email}`
+        return `${this.#nome}, ${this.email}`
     }
 }
 
---
+++ index.js
@@ -0,0 +1,4 @@
+import User from "./User.js";
+
+const novoUser = new User('Mariana','m@m','2024-04-05')
+console.log(novoUser.exibirInfos());
